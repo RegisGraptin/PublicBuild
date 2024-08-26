32471b8dd4e7fbeae82c80f235be24486f0434c0
rere <regis-graptin@sfr.fr>
2024-08-27 00:13:51 +0200

    install tailwindcss

---
+++ front/app/github/page.tsx
@@ -0,0 +1,19 @@
+
+
+
+const GithubPage = () => {
+
+    return (
+        <>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+            </h1>
+            <h2>Projects list</h2>
+            <p>Test</p>
+        </>
+    )
+
+}
+
+
+export default GithubPage;
