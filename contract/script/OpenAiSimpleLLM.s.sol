bb481ff7eb4246f7f89fae1cf882024e7826490d
rere <regis-graptin@sfr.fr>
2024-09-08 00:01:28 +0200

    feat: add sample contract form Galadriel

--- contract/script/OpenAiSimpleLLM.s.sol
+++ contract/script/OpenAiSimpleLLM.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {OpenAiSimpleLLM} from "../src/OpenAiSimpleLLM.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    OpenAiSimpleLLM public openai;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        openai = new OpenAiSimpleLLM();
 
         vm.stopBroadcast();
     }
